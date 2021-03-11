191aa0bf9b23883dd9842d0c12c387646902142f
Prachi Bansal <prachibansal10@gmail.com>
2021-03-11 14:16:02 +0100

    Initial commit

---
+++ src/components/Todo.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Checkbox, IconButton, ListItem, Typography} from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+
+function Todo({ todo, toggleComplete, deleteTodo }) {
+    function handleCheckboxClick() {
+        toggleComplete(todo.id);
+    }
+    function handleDeleteClick() {
+        deleteTodo(todo.id);
+    }
+
+    return (
+        <ListItem style={{ display:"flex" }}>
+            <Checkbox
+                checked={todo.completed} 
+                onClick={handleCheckboxClick} />
+            <Typography
+                style={{ 
+                    textDecoration: todo.completed ? "line-through" : null
+                }}
+            >
+                {todo.task}
+            </Typography>
+            <IconButton onClick={handleDeleteClick}>
+                <CloseIcon />
+            </IconButton>
+        </ListItem>
+    );
+}
+
+export default Todo;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import uuid from 'uuid';
+import {TextField, Button} from "@material-ui/core";
+
+function TodoForm({ addTodo }) {
+    const [todo, setTodo] = useState({
+        id: "",
+        task: "",
+        completed: false
+    });
+
+    function handleTaskInputChange(e) {
+        setTodo({ ...todo, task: e.target.value });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if(todo.task.trim()) {
+            addTodo({ ...todo, id: uuid.v4() });
+            setTodo({ ...todo, task: ""});
+        }
+    }
+
+    return (
+        <form class="todo-form" onSubmit={handleSubmit}>
+            <TextField
+                label="Task" 
+                type="text"
+                name="task"
+                value={todo.task}
+                onChange= {handleTaskInputChange}
+            />
+            <Button type="submit">Add Task</Button>
+        </form>
+    );
+}
+
+export default TodoForm;
